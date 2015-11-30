91896895a8461261f969594fa6a4fa0f0f42124c
li-cindy <li.cindy98@gmail.com>
2015-11-29 22:49:16 -0500

    i hate git

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import google, urllib2, re, bs4
 
-def searchWho(query): 
-    pattern = "[A-Z][a-z]{3,15} [A-Z][a-z]+"
+def searchGeneral(query): 
+    pattern = "[A-Z][a-z]+"
     results = google.search(query, num=10, start=0, stop=10)
     rlist = []
     for url in results: 
@@ -18,8 +18,8 @@ def searchWho(query):
             print "Error. Request could not be processed."
     return rlist[0][0]
 
-def searchWhen(query): 
-    pattern = "[0]?[1-9]|[1][0-2])/([0]?[1-9]|[1|2][0-9]|[3][0|1])/([0-9]{4}|[0-9]{2}"
+def searchWho(query): 
+    pattern = "[A-Z][a-z]{3,15} [A-Z][a-z]+"
     results = google.search(query, num=10, start=0, stop=10)
     rlist = []
     for url in results: 
@@ -29,12 +29,31 @@ def searchWhen(query):
             result = request.read()
             soup = bs4.BeautifulSoup(result, "html.parser")
             raw = soup.get_text()
-            pretty = re.sub("[ \t\n]+", " ",raw)
+            pretty = re.sub("[ \t\n]+", " ", raw)
             entries = re.findall(pattern, pretty)
             rlist.append(entries)
         except: 
             print "Error. Request could not be processed."
-    return rlist
+    return rlist[0][0]
+
+def searchWhen(query): 
+    pattern = "[0-9]?[0-9]/[0-9]?[0-9]/[0-9][0-9]|[A-Z][a-z]*\s[0-9]?[0-9],\s[0-9]{4}"
+    results = google.search(query, num=10, start=0, stop=10)
+    rlist = []
+    for url in results: 
+        print url
+        try:
+            request = urllib2.urlopen(url)
+            result = request.read()
+            soup = bs4.BeautifulSoup(result, "html.parser")
+            raw = soup.get_text()
+            pretty = re.sub("[ \t\n]+", " ", raw)
+            entries = re.findall(pattern, pretty)
+            rlist.append(entries)
+        except: 
+            print "Error. Request could not be processed."
+            
+    return rlist[0][0]
 
 
 
@@ -44,4 +63,4 @@ def Search(query):
     elif "when" in query: 
         return searchWhen(query)
     else:
-        return "Your search must ask a Who or When question! Please rephrase for our sake."
+        return searchGeneral(query)
